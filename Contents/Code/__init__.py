2031464665fb0d646362a733065ab7391feb28b8
shopgirl284 <shopgirl284@gmail.com>
2013-08-16 17:15:51 -0400

    updated code to use carousels

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,6 +1,5 @@
-# DID NOT ADD SORTS BECAUSE THE SITE ALREADY SORTS THE SHOWS BY NAME AND BY LATEST EPISODE FIRST 
-# IF YOU ADD A DATE SORT TO EACH SHOW, IT CAN MESS UP THE ORDER AND PUT THE EPISODES IN THE WRONG 
-# ORDER BECAUSE OFTEN MULTIPLE EPISODE OR VIDEOS ARE UPLOADED ON AND HAVE THE SAME DATE
+# DID NOT ADD SORTS BECAUSE THE SITE ALREADY SORTS THE SHOWS BEST AND A DATE SORT CAN MESS UP THE ORDER 
+# SINCE OFTEN MULTIPLE VIDEOS ARE UPLOADED ON OR HAVE THE SAME DATE
 TITLE    = 'Yahoo Screen'
 PREFIX   = '/video/yahooscreen'
 # NOT SURE IF CODE BELOW IS NEEDED ANY LONGER
@@ -14,73 +13,92 @@ http = 'http:'
 # These variables pull the list id and content id from page
 RE_LIST_ID = Regex('listId: "(.+?)", pagesConfig: ')
 RE_CONTENT_ID = Regex('CONTENT_ID = "(.+?)";')
-# This is the carousel url and JSON data urls
-Carousel = 'http://screen.yahoo.com/_xhr/carousel/bcarousel-mixed-list/?list_id=%s&thumb_ratio=16x9&pyoff=0&title_lines_max=2&show_cite=&show_date=0&show_provider=0&show_author=&show_duration=0&show_subtitle=&show_provider_links=&apply_filter=&filters=&template=tile&num_cols=4&num_rows=8&start_initial=1&max_items=19&pages_per_batch=1&sec=&module=MediaBCarouselMixedLPCA&spaceid=792884066&mod_units=24&renderer_key='
 # This is a global variable for the parameters of the Yahoo JSON data file. Currently it returns 32 items. 
 # To add more returned results, add the last number plus 5 to pc_starts and ",1u-1u-1u-1u-1u" to pc_layouts for each five entries you want to add
 YahooJSON = 'http://screen.yahoo.com/_xhr/slate-data/?list_id=%s&start=0&count=50&pc_starts=1,6,11,16,21,26&pc_layouts=1u-1u-1u-1u-1u,1u-1u-1u-1u-1u,1u-1u-1u-1u-1u,1u-1u-1u-1u-1u,1u-1u-1u-1u-1u,1u-1u-1u-1u-1u'
+RE_CAROUSEL = Regex('Y.Media.BCarousel(.+?),Y.Media.pageChrome')
 
 ###################################################################################################
 def Start():
 
   ObjectContainer.title1 = TITLE
-  HTTP.CacheTime = CACHE_1DAY 
+  HTTP.CacheTime = CACHE_1HOUR 
 
 ###################################################################################################
-@handler(PREFIX, TITLE)
 # There are not separate pages for each section and the main originals page has carousels, 
-# so to get all the shows for each section, sections are hard coded in with a section carousel id
-def MainMenu():
+# so we pull the carousel info into JSON format and use that to pull sections and urls
+@handler(PREFIX, TITLE)
+def MainMenuBest():
 
   oc = ObjectContainer()
 
-# Made special menu for Burning Love
+# Made special menu for Burning Love since it is the most popular show on the site
   url = 'http://screen.yahoo.com/burning-love/'
-  page = HTML.ElementFromURL(url)
+  page = HTML.ElementFromURL(url, cacheTime = CACHE_1DAY)
   title = page.xpath("//head//meta[@property='og:title']//@content")[0]
   summary = page.xpath("//head//meta[@name='description']//@content")[0]
-  thumb = GetThumb(title)
+  thumb = page.xpath('//img[@alt="Burning Love"]//@style')[0]
+  thumb = thumb = thumb.replace("background-image:url('", '').replace("');", '')
 	
-  oc.add(DirectoryObject(
-    key=Callback(BurningLove, title=title, url=url, thumb=thumb), 
-    title=title, 
-    thumb=thumb,
-    summary=summary))
+  oc.add(DirectoryObject(key=Callback(BurningLove, title=title, url=url, thumb=thumb), title=title, thumb=Resource.ContentsOfURLWithFallback(thumb), summary=summary))
 
-  oc.add(DirectoryObject(
-    key=Callback(SectionYahoo, title='Yahoo! Comedy Originals', id='c59ae629-33fc-4bca-977e-604da14af38f'),
-    title='Yahoo! Comedy Originals'))
+  # Here we start the pulls for the other sections
+  content = HTTP.Request(YahooOrigURL).content
+  details = GetYahooJson(content)
+  #Log('the value of details is %s' %details)
 
-  oc.add(DirectoryObject(
-    key=Callback(SectionYahoo, title='Yahoo! Living Originals', id='4b39dfc3-59df-4a8d-b0ca-55380324501c'),
-    title='Yahoo! Living Originals'))
+  i=0
+  for items in details:
+    i=i+1
+    carousel_type = items[i]['modId']
+    carousel_url = items[i]['xhrUrl']
+    carousel_url = YahooURL + carousel_url
+    # To make sure it produces all the results we have to increase the &num_rows=. (changing the &num_cols decreases image size)
+    # deleting data after num_rows= does not seem to affect results so we increase num_rows to 10 with
+    carousel_url = carousel_url.split('&num_rows=')[0] + '&num_rows=10'
+    # Once we pull the type and url from the json, we need to compare it to carousel id on the page
+    page = HTML.ElementFromString(content)
+    # XPATH ON YAHOO ORIGINALS PAGE IS NOT SPECIFIC ENOUGH TO ONLY PULL THE SECTIONS INFO
+    # THE CODE BELOW NARROWS IT DOWN AS MUCH AS POSSIBLE
+    for section in page.xpath('//div[contains(@class,"yom-stage")]/div/div/div'):
+      # NOT ALL SECTIONS HAVE CAROUSEL INFO SO HAD TO PUT IT IN A TRY
+      try:
+        carousel_id = section.xpath('./div[contains(@id,"mediabcarouselmixedlpca")]//@id')[0]
+      except:
+        continue
+      if carousel_id == carousel_type:
+        title = section.xpath('./div[contains(@id,"categoryHeader")]/div/div/h1//text()')[0]
+        oc.add(DirectoryObject(key=Callback(SectionYahoo, title=title, url=carousel_url), title=title))
 
-  oc.add(DirectoryObject(
-    key=Callback(SectionYahoo, title='Yahoo! News Originals', id='ca63a6b9-0e5c-4f61-be53-3803c8461ab9'),
-    title='Yahoo! News Originals'))
+  if len(oc) < 1:
+    return ObjectContainer(header="Empty", message="This directory appears to be empty. There are no shows to display right now.")      
+  else:
+    return oc
 
-  oc.add(DirectoryObject(
-    key=Callback(SectionYahoo, title='Yahoo! Sports Originals', id='66e3b217-e86f-40d1-a8ff-31902550c962'),
-    title='Yahoo! Sports Originals'))
+#########################################################################################################
+# This function pulls carousel data from the Yahoo Originals page using regex and puts it in json format for easier pulls of info
+@route(PREFIX + '/getyahoojson')
+def GetYahooJson(content):
 
-  oc.add(DirectoryObject(
-    key=Callback(SectionYahoo, title='Yahoo! Finance Originals', id='e86e7ba3-9f77-4085-8606-b507b6e409e2'),
-    title='Yahoo! Finance Originals'))
+  data_list = RE_CAROUSEL.findall(content)
+  data_list_new = []
+  x = 1
+  for data in data_list:
+    data = str(x) + ':' + data.lstrip('(')
+    data_list_new.append(data)
+    x=x+1
+  data_json = '[{' + ('},{'.join(data_list_new)) + '}]'
+  details = JSON.ObjectFromString(data_json)
 
-  oc.add(DirectoryObject(
-    key=Callback(SectionYahoo, title='Yahoo! Entertainment Originals', id='94aec3ed-1cf9-41bb-8d59-408a8410fc3a'),
-    title='Yahoo! Entertainment Originals'))
-
-  return oc
-
+  return details
+ 
 ###################################################################################################
 # This function uses the carousel file to pull the the shows for each sections
 @route(PREFIX + '/sectionyahoo')
-def SectionYahoo(title, id):
+def SectionYahoo(title, url):
 
   oc = ObjectContainer(title2=title)
-  list_URL = Carousel %id
-  page = HTML.ElementFromURL(list_URL)
+  page = HTML.ElementFromURL(url)
   for show in page.xpath('//div[@class="item-wrap"]'):
 
       title = show.xpath('./div/p[@class="title"]/a//text()')[0]
@@ -120,9 +138,8 @@ def BurningLove(title, url, thumb):
   return oc
 
 ###################################################################################################
-# This function pulls the ID for the JSON data url
-# All shows have a content ID except one (Animal All Stars), so pull List ID for that one
-# and check for List ID first.
+# This function pulls the ID for the JSON data url. All shows have a content ID except Animal All Stars that has a List ID
+# So check for List ID first.
 @route(PREFIX + '/yahooid')
 def YahooID(url):
 
@@ -216,21 +233,3 @@ def MoreVideosYahoo(title, url):
   else:
     return oc
 
-#############################################################################################################################
-# This function pulls the thumb from a the Yahoo Originals main page
-# It cannot go through all selections since they are in a carousel, but picks up most.  
-# Would require the Yahoo section name and its id to pull the full list from the carousel
-@route(PREFIX + '/getthumb')
-def GetThumb(title):
-
-  try:
-    thumb_page = HTML.ElementFromURL(YahooOrigURL)
-    try:
-      thumb = thumb_page.xpath('//a[@class="media"]/img[@alt="%s"]//@style' % title)[0]
-    except:
-      thumb = thumb_page.xpath('//a[@class="img-wrap"]/img[@alt="%s"]//@style' % title)[0]
-    thumb = thumb.replace("background-image:url('", '').replace("');", '')
-  except:
-    thumb = R(ICON)
-
-  return thumb
