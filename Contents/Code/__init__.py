619497363d0b1debd5a7e487430bc2eabc91dd52
shopgirl284 <shopgirl284@gmail.com>
2013-10-30 21:19:47 -0400

    took put extra code

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -9,9 +9,6 @@ YahooURL = 'http://screen.yahoo.com'
 YahooSectionJSON = 'http://screen.yahoo.com/ajax/resource/channels;videocount=0;count=%s;type=%s'
 YahooShowJSON = 'http://screen.yahoo.com/ajax/resource/channel/id/%s;count=20;start=%s'
 YahooShowURL = 'http://screen.yahoo.com/%s/%s.html'
-# There is an autocomplete for the yahoo search at 'http://screen.yahoo.com/_xhr/search-autocomplete/?query=' 
-# It returns results of type keys so not surewhat I would use it for
-SearchJSON = 'http://video.search.yahoo.com/search//?p=%s&fr=screen&o=js&gs=0&b=%s'
 
 ###################################################################################################
 def Start():
@@ -36,7 +33,6 @@ def MainMenu():
   # Most Popular on Yahoo Screens
   oc.add(DirectoryObject(key=Callback(VideoJSON, title='Most Popular Videos', url='popular'), title='Most Popular Videos'))
   # Yahoo Search Object
-  #oc.add(InputDirectoryObject(key=Callback(SearchYahoo, title='Search Yahoo Screen Videos'), title='Search Yahoo Screen Videos', summary="Click here to search for Videos on Yahoo Screen", prompt="Search for videos in Yahoo Screen"))
   oc.add(SearchDirectoryObject(identifier="com.plexapp.plugins.yahooscreen", title=L("Search Yahoo Screen Videos"), prompt=L("Search for Videos")))
 
   return oc
@@ -136,8 +132,6 @@ def VideoJSON(title, url, start=0):
       thumb = video['thumbnails'][1]['url']
     except:
       thumb = R(ICON)
-    #thumb = video['thumbnails'][1]['url']
-    Log('the value of thumb is %s' %thumb)
     # May need this for excluding videos that may not work with URL service
     provider_name = video['provider_name']
 
@@ -158,54 +152,5 @@ def VideoJSON(title, url, start=0):
           
   if len(oc) < 1:
     return ObjectContainer(header="Empty", message="This directory appears to be empty or contains videos that are not compatible with this channel.")      
-  else:
-    return oc
-######################################################################################################
-# This functions searches Yahoo Screens at 'http://video.search.yahoo.com/search//?fr=screen&q=love'
-@route(PREFIX + '/searchyahoo', b=int)
-def SearchYahoo(title, query='plex', b=1):
-
-  oc = ObjectContainer(title2=title)
-  # When searching on their site, the resulting URL has pluses instead of %20 for spaces though both usePlus options work
-  JSON_url = SearchJSON %(String.Quote(query, usePlus = True), b)
-  try:
-    data = JSON.ObjectFromURL(JSON_url)
-  except:
-    return ObjectContainer(header=L('Error'), message=L('This feed does not contain any video'))
-
-  x=0
-  if data.has_key('results'):
-    for entry in data['results']:
-      x=x+1
-      search_data = HTML.ElementFromString(entry)
-      url = search_data.xpath('//a//@data-rurl')[0]
-      thumb = search_data.xpath('//a/img//@src')[0]
-      title = search_data.xpath('//a/div/div/h3//text()')[0]
-      duration = Datetime.MillisecondsFromString(search_data.xpath('//a/div/span//text()')[0])
-      summary_info = search_data.xpath('//a//@data')[0]
-      summary_data = JSON.ObjectFromString(summary_info)
-      summary = summary_data['d']
-        
-      if not url.startswith('http://'):
-        url = YahooURL + url
-
-      # had one give no service error with cbs in url
-      if 'cbs.html' not in url:
-        oc.add(VideoClipObject(
-          url = url, 
-          title = title, 
-          thumb = Resource.ContentsOfURLWithFallback(thumb),
-          summary = summary,
-          duration = duration))
-
-# Paging code. Each page pulls 30 results use x counter for need of next page
-  if x >= 30:
-    b = b + 30
-    oc.add(NextPageObject(
-      key = Callback(SearchYahoo, title = title, b=b, query = query), 
-      title = L("Next Page ...")))
-	
-  if len(oc) < 1:
-    return ObjectContainer(header="Empty", message="This directory appears to be empty. There are no videos to display right now.")      
   else:
     return oc
